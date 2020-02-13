ecd01d7e3143f4670a4cbca376aa95eb0a703175
Kevin Villalobos <kevinv15v@gmail.com>
2020-02-13 13:19:49 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,93 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_reader :species, :name
+  attr_accessor :dog, :cat, :pets
+  
+@@all = []
+
+  def initialize(name, pets = {:dogs => [], :cats => []})
+    @name = name
+    @pets = pets
+    @@all << self
+    @species = "human"
+    
+  end 
+  
+ def say_species
+   "I am a #{self.species}."
+ end
+
+  def self.all
+    
+    @@all
+  end
+  
+  def self.count
+    
+    self.all.size
+  end
+  
+  def self.reset_all
+    
+    self.all.clear
+    
+  end
+  
+  def cats
+    Cat.all.select do |cat| 
+      cat.owner == self
+    end
+
+  end
+  
+  def dogs
+   Dog.all.select do |dog| 
+      dog.owner == self
+    end
+
+  end
+  
+  def buy_cat(name)
+    
+    cat = Cat.new(name, self)
+    
+  end
+  
+  def buy_dog(name)
+    
+    dog = Dog.new(name, self)
+    
+  end
+    
+    def walk_dogs
+    Dog.all.each do |dog|
+      dog.mood = 'happy'
+    end
+  end
+
+  def feed_cats
+    Cat.all.each do |cat|
+      cat.mood = 'happy'
+    end
+  end
+
+
+    
+
+  def sell_pets
+    @pets.each do |type, name_array|
+      name_array.each do |pet|
+        pet.mood = "nervous"
+        name_array.delete(pet)
+      end 
+    end
+    @pets = {}
+  end
+  
+
+  def list_pets
+    "I have #{Dog.all.length} dog(s), and #{Cat.all.length} cat(s)."
+  end
+
 end
